f55229dacd81b19c5a75e5fcdb5d9967658bc467
Spax19 <Kmotsepe807@gmail.com>
2023-02-19 19:31:39 +0200

    Positive or Ngative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,15 +4,15 @@
 #include <time.h>
 /* more headers goes there */
 
-/**
+
+/*
  * main - Entry point
  * 
- *
  * Description: 'the program's description'
  * 
- *
  * Return: Always 0 (Success)
- */
+*/
+
 
 /* betty style doc for function main goes there */
 int main(void)
