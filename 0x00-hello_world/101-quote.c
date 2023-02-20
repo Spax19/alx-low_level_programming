06994ed5e3207cab7578df8458b19a0665ec0e53
Spax19 <Kmotsepe807@gmail.com>
2023-02-20 23:34:47 +0200

    changes

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+
 /**
  * main - Entry point
  * Return: Always 1 (Success)
  */
-int main(void)
-{
-	write(STDOUT_FILENO, "and that piece of art is useful
-			\" - Dora Korpar, 2015-10-19\n", 59);
-	return (1);
-}
 
+int main() {
+  const char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+  const int length = sizeof(message) - 1;
+  const ssize_t bytes_written = write(STDERR_FILENO, message, length);
+  return (bytes_written == length) ? 1 : 0;
+}
