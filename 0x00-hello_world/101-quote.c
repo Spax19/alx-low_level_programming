da226a3eeba97049d647e96c0276d49ba0f1c704
Spax19 <Kmotsepe807@gmail.com>
2023-02-20 23:37:38 +0200

    Changes

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,9 +7,13 @@
  * Return: Always 1 (Success)
  */
 
-int main() {
+int main(void) 
+{
   const char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
   const int length = sizeof(message) - 1;
+
   const ssize_t bytes_written = write(STDERR_FILENO, message, length);
+
   return (bytes_written == length) ? 1 : 0;
 }
