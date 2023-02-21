49b60a8d6033987509d64929c7e6d93e3845b364
Spax19 <Kmotsepe807@gmail.com>
2023-02-21 14:51:52 +0200

    _putchar

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -11,5 +11,4 @@ void times_table(void);
 int add(int a, int b);
 void print_to_98(int n);
 void print_times_table(int n);
-void putformat(int);
-~                                                                                                                       ~                       
+void putformat(int);                      
