ddbca302af7a68a30a5fc164d2d9fb6fab4a85b5
Spax19 <Kmotsepe807@gmail.com>
2023-02-21 17:31:23 +0200

    changes

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -8,15 +8,15 @@
  * Return: Always 0 (Success)
  */
 
-int main(void) 
-{
-    char c;
+int main (void) 
+{	
+	char c;
 
-    for (c = 'a'; c <= 'z'; c++)
-    {
-	putchar(c);
-    	putchar('\n');
-    }
-    return 0;
+    	for (c = 'a'; c <= 'z'; c++)
+    	{
+		putchar(c);
+    		putchar('\n');
+    	}
+    	return 0;
 	
 }
