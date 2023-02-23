a0aa13c7b9eac5c96a92e69ddb13fe99e6629d39
Spax19 <Kmotsepe807@gmail.com>
2023-02-23 16:16:25 +0200

    Mul

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * _mul -  a function that multiples two numbers
+ * @c: input
+ * Return: results
+ */
+
+int mul(int a, int b)
+{
+	int result = a * b;
+	return result;
+}
