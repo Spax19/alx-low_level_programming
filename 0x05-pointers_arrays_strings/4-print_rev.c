0b795ef5b88b2a0e4920f82ddfb8a2d3f1dcf58c
Spax19 <Kmotsepe807@gmail.com>
2023-03-01 18:26:54 +0200

    String in reverse

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -9,12 +9,13 @@
  */
 void print_rev(char *s)
 {
-    int len = strlen(s);
+	int len = strlen(s);
+	int i;
 
-    for(int i = len - 1; i >= 0; i--)
-    {
-        printf("%c", s[i]);
-    }
-    printf("\n");
+	for(i = len - 1; i >= 0; i--)
+	{
+		printf("%c", s[i]);
+	}
+	printf("\n");
 }
 
