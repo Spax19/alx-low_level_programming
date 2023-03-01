e0d05547fb82739f7ea9e5ad9a4b68c2bb1bc546
Spax19 <Kmotsepe807@gmail.com>
2023-03-01 19:50:57 +0200

    Multiply two integers

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -3,15 +3,12 @@
 
 /**
  * _mul -  a function that multiples two numbers
+ *
  * @c: input
+ *
  * Return: result of the two numbers
  */
-int numbers(int a, int b);
-
-int mul() {
-    int x = 5;
-    int y = 7;
-    int z = numbers((x, y);
-    printf("%d * %d = %d\n", x, y, z);
-    return 0;
+int mul(int a, int b) 
+{
+	return a * b;
 }
