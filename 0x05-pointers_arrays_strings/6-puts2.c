b97d39dbe1435a3c770aa36e8b733f8a5745f814
Spax19 <Kmotsepe807@gmail.com>
2023-03-01 18:53:22 +0200

    Characters of a string

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * puts2 - prints every other character of a string starting with the first one
+ *
+ * @str: input
+ *
+ * Return: str.
+ */
+void puts2(char *str)
+{
+    int i = 0;
+
+    while (str[i] != '\0')
+    {
+        printf("%c", str[i]);
+        i += 2;
+    }
+    printf("\n");
+}
