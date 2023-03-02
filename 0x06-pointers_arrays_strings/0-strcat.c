8daad02ec872a3252284f2a3ea2ecb4fdeb79f28
Spax19 <Kmotsepe807@gmail.com>
2023-03-02 21:33:13 +0200

    Concatenate two strings

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strcat - concatenates two strings
+ *
+ * @src: input
+ *
+ * Return: dest
+ */
+char* _strcat(char* dest, const char* src) 
+{
+    char* p = dest;
+
+    while (*p != '\0') 
+    {
+        p++;
+    }
+    while (*src != '\0') 
+    {
+        *p = *src;
+        p++;
+        src++;
+    }
+
+    *p = '\0';
+    return dest;
+}
