c39f567a943667768dc62d4d3ae6ec6f6b2d9afb
Spax19 <Kmotsepe807@gmail.com>
2023-03-06 20:44:12 +0200

    changes

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -9,30 +9,42 @@
 * @year: year
 * Return: void
 */
-
 void print_remaining_days(int month, int day, int year)
 {
-    if ((year % 4 == 0 || year % 400 == 0) && !(year % 100 == 0))
-    {
-        if (month >= 2 && day >= 60)
-        {
-            day++;
-        }
+    int days_in_month[] = {31, 28 + is_leap_year(year), 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+    int day_of_year = 0;
+    int remaining_days = 0;
 
-        printf("Day of the year: %d\n", day);
-        printf("Remaining days: %d\n", 366 - day);
+    // Check for invalid dates
+    if (month < 1 || month > 12 || day < 1 || day > days_in_month[month - 1])
+    {
+        printf("Invalid date: %02d/%02d/%04d\n", month, day, year);
+        return;
+    }
+
+    // Calculate day of year
+    for (int i = 0; i < month - 1; i++)
+    {
+        day_of_year += days_in_month[i];
+    }
+    day_of_year += day;
+
+    // Calculate remaining days
+    remaining_days = 365 + is_leap_year(year) - day_of_year;
+
+    printf("Day of the year: %d\n", day_of_year);
+    printf("Remaining days: %d\n", remaining_days);
+}
+
+int is_leap_year(int year)
+{
+    if (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0))
+    {
+        return 1;
     }
     else
     {
-        if (month == 2 && day == 60)
-        {
-            printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
-        }
-        else
-        {
-            printf("Day of the year: %d\n", day);
-            printf("Remaining days: %d\n", 365 - day);
-        }
+        return 0;
     }
 }
 
