20043673e1a1da688f263b06f6ec9a0c4b046343
Spax19 <Kmotsepe807@gmail.com>
2023-03-06 18:11:44 +0200

    strspn

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * _strspn - gets the length of a prefix substring.
+ *
+ * Return: s
+ */
+
+int _strspn(char *s, char *accept) 
+{
+    	int count = 0;
+    	char *p = s;
+    	while (*p != '\0' && strchr(accept, *p) != NULL) 
+    	{
+        	count++;
+        	p++;
+    	}
+    	return count;
+}
