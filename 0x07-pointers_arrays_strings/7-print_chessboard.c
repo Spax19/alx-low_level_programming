a000be7fddbcfbcaee622e83c67791f261fe5159
Spax19 <Kmotsepe807@gmail.com>
2023-03-06 18:50:03 +0200

    Print chessboard

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * print_chessboard - prints out chessboard
+ *
+ * Return: 0.
+ */
+
+void print_chessboard(char (*a)[8]) 
+{
+
+    	for (int i = 0; i < 8; ++i) 
+	{
+        	for (int j = 0; j < 8; ++j) 
+		{
+            		std::cout << a[i][j] << ' ';
+        	}	
+        	std::cout << std::endl;
+    	}
+}
