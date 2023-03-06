d35a7fb6110c26cd2c35a868a8888828169136a3
Spax19 <Kmotsepe807@gmail.com>
2023-03-06 17:57:25 +0200

    strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * _strchr -  locates a character in a string.
+ *
+ * @s: the string
+ *
+ * Return: c
+ */
+
+char *_strchr(char *s, char c) 
+{
+
+    	while (*s != '\0') 
+    	{
+        	if (*s == c) 
+		{
+            		return s;
+        	}
+        	s++;
+    	}
+    	if (c == '\0') 
+    	{
+        	return s;
+    	}
+    	return NULL;
+}
