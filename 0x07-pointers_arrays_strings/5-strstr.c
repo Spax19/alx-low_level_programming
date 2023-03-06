fdbca34fc1f693dcd4d4d35b1cd2362255346210
Spax19 <Kmotsepe807@gmail.com>
2023-03-06 18:39:14 +0200

    strstr

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * _strstr -  locates a substring.
+ *
+ * Return: haystack
+ */
+
+char *_strstr(char *haystack, char *needle) 
+{
+
+    	while (*haystack != '\0') 
+	{
+        	char *h = haystack;
+        	char *n = needle;
+        	while (*n != '\0' && *h == *n) 
+		{
+            		h++;
+            		n++;
+        	}
+        	if (*n == '\0') 
+		{
+            		return haystack;
+        	}
+        	haystack++;
+    	}
+    	return NULL;
+}
+
